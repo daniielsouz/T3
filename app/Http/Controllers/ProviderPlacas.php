2f2d4cd9b7679d108547af9b3fb23ff797641284
daniielsouz <danielsouz.dev@gmail.com>
2024-08-25 11:52:29 -0300

    .

---
+++ app/Http/Controllers/ProviderPlacas.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProviderPlacas extends Controller
+{
+    public function ProviderPlacas(){
+        return 'Isso é uma placa';
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ProviderPlacas::class,
+    App\Providers\ProviderUser::class,
+    App\Providers\teste::class,
 ];
